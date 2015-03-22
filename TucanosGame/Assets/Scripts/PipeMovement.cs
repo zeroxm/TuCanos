9bf2480828f3c08808425479856bb3e2a5e63bd0
zeroxm <zeroxm@hotmail.com>
2015-03-22 19:57:17 -0300

    Terminado PickUp e PutDown

--- TucanosGame/Assets/Scripts/PipeMovement.cs
+++ TucanosGame/Assets/Scripts/PipeMovement.cs
@@ -28,11 +28,27 @@ public class PipeMovement : MonoBehaviour {
 	}
 
 	public bool putDown(Vector3 currentPosition, Quaternion currentRotation){
-		float x = Mathf.Round(currentPosition.x);
-		float y = Mathf.Round(currentPosition.y);
-		float z = Mathf.Round(currentPosition.z);
-		if (currentRotation.Equals (new Quaternion (0, 1.0f, 0, 0))) {
-			print("Cima");
+		togglePicked ();
+		float x = Mathf.Round (currentPosition.x);
+		float y = Mathf.Round (currentPosition.y);
+		float z = Mathf.Round (currentPosition.z);
+
+		int angle = Mathf.RoundToInt (Quaternion.Angle (currentRotation, new Quaternion (0, 1.0f, 0, 0)));
+		if (angle==0) {
+			this.transform.position = new Vector3(x,y,z+1);
+		}
+		else if(angle==180){
+			this.transform.position = new Vector3(x,y,z-1);
+		}
+		else{
+			angle = Mathf.RoundToInt (Quaternion.Angle (currentRotation, new Quaternion (0, 0.7f, 0, 0.7f)));
+		
+		if (angle < 90) {
+			this.transform.position = new Vector3(x-1,y,z);
+		}
+		if (angle > 90) {
+			this.transform.position = new Vector3(x+1,y,z);
+		}
 		}
 		return true;
 	}
