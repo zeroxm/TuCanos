db443fb0dd1ea9a8839ec7688d934e3de7c9520e
victorganciar <victor_ganciar23@hotmail.com>
2015-03-23 20:42:59 -0300

    Adicionados menus

---
+++ TucanosGame/Assets/Scripts/GameOver.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+var posicaoX: float;
+var posicaoY: float;
+var altura:   float;
+var largura:  float;
+
+var textoScore: UI.Text;
+
+var skin: GUISkin;
+
+function Start () {
+	largura = 100;
+	altura = 20;
+	posicaoX = Screen.width/2 - largura/2;
+    posicaoY = Screen.height/2 - altura/2;
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+
+    GUI.skin = skin;
+    
+	if(GUI.Button(Rect(posicaoX,posicaoY,largura,altura),"Recomeçar")){
+		Application.LoadLevel("MenuPrincipal");
+	}
+	
+	if(GUI.Button(Rect(posicaoX,posicaoY + 25,largura,altura),"Sair")){
+		Application.Quit();
+	}
+}
---
+++ TucanosGame/Assets/Scripts/Menu.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var posicaoX: float;
+var posicaoY: float;
+var altura:   float;
+var largura:  float;
+
+var skin: GUISkin;
+
+function Start () {
+	largura = 300;
+	altura = 60;
+	posicaoX = Screen.width/2 - largura/2;
+    posicaoY = Screen.height/2 - altura/2;
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+
+    GUI.skin = skin;
+    
+	if(GUI.Button(Rect(posicaoX,posicaoY + 25,largura,altura),"Start")){
+		Application.LoadLevel("Level_01");
+	}
+	
+	if(GUI.Button(Rect(posicaoX,posicaoY + 100,largura,altura),"Exit")){
+		Application.Quit();
+	}
+}
