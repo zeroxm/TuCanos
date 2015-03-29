5b36ffa072722792d84fb029df1262217f098869
victorganciar <victor_ganciar23@hotmail.com>
2015-03-28 21:30:38 -0300

    Ajustados menus principal e pause

--- TucanosGame/Assets/Scripts/Menu.js
+++ TucanosGame/Assets/Scripts/Menu.js
@@ -9,7 +9,7 @@ var skin: GUISkin;
 
 function Start () {
 	largura = 300;
-	altura = 60;
+	altura = 55;
 	posicaoX = Screen.width/2 - largura/2;
     posicaoY = Screen.height/2 - altura/2;
 }
@@ -22,11 +22,19 @@ function OnGUI(){
 
     GUI.skin = skin;
     
-	if(GUI.Button(Rect(posicaoX,posicaoY + 25,largura,altura),"Start")){
+	if(GUI.Button(Rect(posicaoX,posicaoY - 45,largura,altura),"1p Game")){
 		Application.LoadLevel("Level_01");
 	}
 	
-	if(GUI.Button(Rect(posicaoX,posicaoY + 100,largura,altura),"Exit")){
+	if(GUI.Button(Rect(posicaoX,posicaoY + 15,largura,altura),"2p Coop")){
+		Application.LoadLevel("Level_01");
+	}
+	
+	if(GUI.Button(Rect(posicaoX,posicaoY + 75,largura,altura),"Time Atack")){
+		Application.LoadLevel("Level_01");
+	}
+	
+	if(GUI.Button(Rect(posicaoX,posicaoY + 135,largura,altura),"Exit")){
 		Application.Quit();
 	}
 }
