f2ea5a2799f58ede3f8395a414f6f26db43b8840
victorganciar <victor_ganciar23@hotmail.com>
2015-04-18 22:33:13 -0300

    Menu Titulo e Level 4
    Adicionada animação no menu título e criado level 4.

--- TucanosGame/Assets/Scripts/MatrixManager.cs
+++ TucanosGame/Assets/Scripts/MatrixManager.cs
@@ -33,21 +33,19 @@ public class MatrixManager : MonoBehaviour {
 				{empty, empty, empty, empty, empty, empty},
 				{verticalPipe, empty, empty, empty, empty, empty},
 				{upRightCorner, horizontalPipe, empty, empty, empty, empty}};
-		} else 
-			if (Application.loadedLevelName.Equals("Level_02", System.StringComparison.OrdinalIgnoreCase)) {
-				matriz = new int[10, 6] 
-				{ 	{start, empty, empty, empty, empty, verticalEnd},
+		} else if (Application.loadedLevelName.Equals("Level_02", System.StringComparison.OrdinalIgnoreCase)) {
+			matriz = new int[10, 6] 
+			{ 	{start, empty, empty, empty, empty, verticalEnd},
 				{empty, empty, rightDownCorner, horizontalPipe, horizontalPipe, empty},
-					{verticalPipe, empty, empty, empty, empty, empty},
-					{upRightCorner, horizontalPipe, empty, empty, empty, empty},
-					{empty, empty, verticalPipe, empty, verticalPipe, empty},
-					{empty, empty, leftUpCorner, empty, verticalPipe, empty},
-					{verticalPipe, empty, empty, empty, empty, empty},
-					{upRightCorner, downleftCorner, empty, empty, empty, empty},
-					{empty, empty, empty, empty, empty, empty},
-					{empty, empty, empty, horizontalPipe, leftUpCorner, empty}};
-			} else
-		if (Application.loadedLevelName.Equals("Level_03", System.StringComparison.OrdinalIgnoreCase)) {
+				{verticalPipe, empty, empty, empty, empty, empty},
+				{upRightCorner, horizontalPipe, empty, empty, empty, empty},
+				{empty, empty, verticalPipe, empty, verticalPipe, empty},
+				{empty, empty, leftUpCorner, empty, verticalPipe, empty},
+				{verticalPipe, empty, empty, empty, empty, empty},
+				{upRightCorner, downleftCorner, empty, empty, empty, empty},
+				{empty, empty, empty, empty, empty, empty},
+				{empty, empty, empty, horizontalPipe, leftUpCorner, empty}};
+		} else if (Application.loadedLevelName.Equals("Level_03", System.StringComparison.OrdinalIgnoreCase)) {
 			matriz = new int[10, 9] 
 			{ 	{start, empty, empty, empty, empty, empty, empty, empty, empty},
 				{empty, empty, empty, horizontalPipe, horizontalPipe, downleftCorner, empty, empty, horizontalEnd},
@@ -59,26 +57,35 @@ public class MatrixManager : MonoBehaviour {
 				{empty, empty, empty, verticalPipe, empty, rightDownCorner, horizontalPipe, leftUpCorner, empty},
 				{empty, empty, empty, verticalPipe, empty, empty, empty, empty, empty},
 				{empty, empty, empty, upRightCorner, horizontalPipe, horizontalPipe, empty, empty, empty}};
-		} else {
-			if (Application.loadedLevelName.Equals("Level_05", System.StringComparison.OrdinalIgnoreCase)) 
-			{
-				matriz = new int[10, 12] 
-				{ 	{start, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty},
-					{verticalPipe, empty, rightDownCorner, horizontalPipe, horizontalPipe, horizontalPipe, empty, empty, empty, empty, empty, empty},
-					{verticalPipe, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty},
-					{empty, horizontalPipe, downleftCorner, empty, empty, empty, verticalPipe, empty, empty, empty, horizontalPipe, empty},
-					{empty, empty, empty, empty, verticalPipe, empty, verticalPipe, empty, empty, empty, empty, upRightEnd},
-					{empty, horizontalPipe, leftUpCorner, empty, verticalPipe, empty, verticalPipe, empty, empty, empty, empty, empty},
-					{empty, empty, empty, empty, empty, empty, empty, empty, verticalPipe, empty, empty, empty},
-					{upRightCorner, downleftCorner, empty, empty, empty, empty, empty, empty, verticalPipe, empty, empty, empty},
-					{empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty},
-					{empty, empty, empty, horizontalPipe, leftUpCorner, empty, upRightCorner, horizontalPipe, leftUpCorner, empty, empty, empty}};
-			}else
-			{
-				Debug.Log("ERRO: MATRIZ DA FASE NAO EXISTE");
-			}
+		} else if (Application.loadedLevelName.Equals("Level_04", System.StringComparison.OrdinalIgnoreCase)) {
+			matriz = new int[10, 9] 
+			{ 	{start, empty, empty, empty, empty, empty, empty, empty, empty},
+				{empty, empty, horizontalPipe, horizontalPipe, horizontalPipe, empty, rightDownCorner, downleftCorner, rightDownEnd},
+				{empty, empty, empty, empty, empty, empty, empty, empty, empty},
+				{empty, empty, empty, empty, empty, verticalPipe, verticalPipe, empty, empty},
+				{empty, empty, rightDownCorner, empty, horizontalPipe, leftUpCorner, upRightCorner, horizontalPipe, empty},
+				{empty, empty, verticalPipe, empty, empty, empty, empty, rightDownCorner, empty},
+				{rightDownCorner, empty, leftUpCorner, empty, empty, empty, empty, verticalPipe, empty},
+				{upRightCorner, empty, empty, empty, empty, empty, empty, empty, empty},
+				{empty, empty, empty, empty, empty, verticalPipe, empty, empty, empty},
+				{empty, empty, empty, empty, empty, upRightCorner, empty, empty, empty}};
+		} else if (Application.loadedLevelName.Equals("Level_05", System.StringComparison.OrdinalIgnoreCase)) {
+			matriz = new int[10, 12] 
+			{ 	{start, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty},
+				{verticalPipe, empty, rightDownCorner, horizontalPipe, horizontalPipe, horizontalPipe, empty, empty, empty, empty, empty, empty},
+				{verticalPipe, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty},
+				{empty, horizontalPipe, downleftCorner, empty, empty, empty, verticalPipe, empty, empty, empty, horizontalPipe, empty},
+				{empty, empty, empty, empty, verticalPipe, empty, verticalPipe, empty, empty, empty, empty, upRightEnd},
+				{empty, horizontalPipe, leftUpCorner, empty, verticalPipe, empty, verticalPipe, empty, empty, empty, empty, empty},
+				{empty, empty, empty, empty, empty, empty, empty, empty, verticalPipe, empty, empty, empty},
+				{upRightCorner, downleftCorner, empty, empty, empty, empty, empty, empty, verticalPipe, empty, empty, empty},
+				{empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty},
+				{empty, empty, empty, horizontalPipe, leftUpCorner, empty, upRightCorner, horizontalPipe, leftUpCorner, empty, empty, empty}};
+		}else {
+			Debug.Log("ERRO: MATRIZ DA FASE NAO EXISTE");
 		}
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
